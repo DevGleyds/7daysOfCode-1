b52b3d5daca359deed8ddb2941fd6fb9fc11b65b
DevGleyds <gleydsongui93@gmail.com>
2024-02-19 05:07:03 -0300

    commitUnic

---
+++ index.js
@@ -0,0 +1,31 @@
+//Nesse código irei compara diferentes tipos de variáveis em JavaScript//
+
+//Variáveis://
+let numeroUm = 1;
+let stringUm = "1";
+let numeroTrinta = 30;
+let stringTrinta = "30";
+let numeroDez = 10;
+let stringDez = "10";
+
+if (numeroUm == stringUm) {
+  console.log(
+    "As variáveis numeroUm e stringUm nos mostra os mesmos valores mas tipos diferente"
+  );
+} else {
+  console.log("As variáveis não tem o mesmo valor");
+}
+if (numeroTrinta == stringTrinta) {
+  console.log(
+    "As variáveis numeroTrinta e stringTrinta tem os mesmos valores mas tipos diferente"
+  );
+} else {
+  console.log("As variáveis não tem o mesmo valor");
+}
+if (numeroDez == stringDez) {
+  console.log(
+    "As variáveis numeroDez e stringDez também tem os mesmos valores mas tipos diferente"
+  );
+} else {
+  console.log("As variáveis não tem o mesmo valor");
+}
